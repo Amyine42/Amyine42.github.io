65528cd8e24896e38e77cdb6484d4188d1778fb0
Amyine42 <132317090+Amyine42@users.noreply.github.com>
2024-12-08 18:21:28 +0100

    Update obstacle.js

--- SpaceGame/obstacle.js
+++ SpaceGame/obstacle.js
@@ -1,22 +1,3 @@
-// class Obstacle {
-//   constructor(x, y, r, couleur) {
-//     this.pos = createVector(x, y);
-//     this.r = r;
-//     this.color = couleur;
-//   }
-
-//   show() {
-//     push();
-//     fill(this.color);
-//     stroke(0)
-//     strokeWeight(3);
-//     ellipse(this.pos.x, this.pos.y, this.r * 2);
-//     fill(0);
-//     ellipse(this.pos.x, this.pos.y, 10);
-//     pop();
-//   }
-// }
-
 class Obstacle {
   constructor(x, y, r, image) {
     this.pos = createVector(x, y);
